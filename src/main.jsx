0492d0d84266ef295d1cbdecc00164b13331a837
Hernane <hernanejosepereira@hotmail.com>
2026-08-27 15:36:41 -0300

    App.tsx rodando com CSS global no index.css

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import App from "./App.jsx";
+import App from "./App.tsx";
 import "./index.css";
 
 // React Query caches/coordinates the Supabase (server-state) calls used by
